14d12d2ca8847db5860eefbceb6cef15724098c7
DESKTOP-H2UVHVF\PSK GAMING <ainuttaqimzawaid1000@gmail.com>
2023-08-10 09:14:16 +0700

    menambah api update dan delete

--- app/product_v4/controller.js
+++ app/product_v4/controller.js
@@ -1,9 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 const Product = require('./model');
+const { ObjectId } = require('mongodb');
 
 const index = (req, res) => {
     Product.find()
+        .then(result => res.send({
+            status: 'success',
+            Response: result
+        }))
+        .catch(error => res.send(error));
+}
+
+const view = (req, res) => {
+    const id = req.params;
+    Product.findOne({ _id: new ObjectId(id) })
         .then(result => res.send({
             status: 'success',
             Response: result
@@ -26,7 +37,35 @@ const store = (req, res) => {
     }
 }
 
+const update = (req, res) => {
+    const { name, price, stock, status } = req.body;
+    const image = req.file;
+    if (image) {
+        const target = path.join(__dirname, '../../uploads', image.originalname);
+        fs.renameSync(image.path, target);
+        Product.updateOne({ _id: new ObjectId(req.params.id) }, { $set: { name, price, stock, status, image_url: `http://localhost:3000/public/${image.originalname}` } })
+            .then(result => res.send({
+                status: 'success',
+                Response: result
+            }))
+            .catch(error => res.send(error));
+    }
+}
+
+const destroy = (req, res) => {
+    const id = req.params;
+    Product.deleteOne({ _id: new ObjectId(id) })
+        .then(result => res.send({
+            status: 'success',
+            Response: result
+        }))
+        .catch(error => res.send(error));
+}
+
 module.exports = {
     index,
-    store
+    view,
+    store,
+    update,
+    destroy
 };
--- app/product_v4/routes.js
+++ app/product_v4/routes.js
@@ -4,9 +4,9 @@ const upload = multer({ dest: 'uploads' });
 const productController = require('./controller');
 
 router.get('/product', productController.index);
-// router.get('/product/:id', productController.view);
+router.get('/product/:id', productController.view);
 router.post('/product', upload.single('image'), productController.store);
-// router.put('/product/:id', upload.single('image'), productController.update);
-// router.delete('/product/:id', upload.single('image'), productController.destroy);
+router.put('/product/:id', upload.single('image'), productController.update);
+router.delete('/product/:id', upload.single('image'), productController.destroy);
 
 module.exports = router;
